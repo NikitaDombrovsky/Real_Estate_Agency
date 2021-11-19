0e274f3befbe4c3f9e95e9f0e9d836f6cdd46b7d
DombrovskyPR-318 <nikita.nikita55@mail.ru>
2021-11-19 13:03:29 +0600

    stone free

---
+++ SupplyEditPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Agency
+{
+    /// <summary>
+    /// Логика взаимодействия для SupplyEditPage.xaml
+    /// </summary>
+    public partial class SupplyEditPage : Page
+    {
+        public Supply _supply = new Supply();
+        public SupplyEditPage(Supply selectedsupply)
+        {
+            InitializeComponent();
+            if (selectedsupply != null)
+                _supply = selectedsupply;
+            DataContext = _supply;
+            ComboAgent.ItemsSource = Real_estate_agencyEntities2.GetContext().Supplies.ToList();
+            ComboClient.ItemsSource = Real_estate_agencyEntities2.GetContext().Supplies.ToList();
+            ComboDistricts.ItemsSource = Real_estate_agencyEntities2.GetContext().Supplies.ToList();
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_supply.Id == 0)
+                Real_estate_agencyEntities2.GetContext().Supplies.Add(_supply);
+            try
+            {
+                Real_estate_agencyEntities2.GetContext().SaveChanges();
+                MessageBox.Show("Добавление успешно!");
+                App.ParentWindowRef.ParentFrame.Navigate(new SupplyPage());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+
+            }
+        }
+    }
+}
--- SupplyPage.xaml.cs
+++ SupplyPage.xaml.cs
@@ -20,35 +20,36 @@ namespace Agency
     /// </summary>
     public partial class SupplyPage : Page
     {
-        private Supply_View _supplyView = new Supply_View();
+        private Supply _supplyView = new Supply();
         public SupplyPage()
         {
             InitializeComponent();
+            
             DataContext = _supplyView;
-            supplyDataGrid.ItemsSource = Real_estate_agencyEntities2.GetContext().Supply_View.ToList();
+            supplyDataGrid.ItemsSource = Real_estate_agencyEntities2.GetContext().Supplies.ToList();
         }
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
-            App.ParentWindowRef.ParentFrame.Navigate(new ClientEditPage(null));
+            App.ParentWindowRef.ParentFrame.Navigate(new SupplyEditPage(null));
         }
         private void BtnEdit_Click(object sender, RoutedEventArgs e)
         {
-            App.ParentWindowRef.ParentFrame.Navigate(new ClientEditPage((sender as Button).DataContext as Client));
+            App.ParentWindowRef.ParentFrame.Navigate(new SupplyEditPage((sender as Button).DataContext as Supply));
         }
 
         private void BtnDel_Click(object sender, RoutedEventArgs e)
         {
-            var supplyForRemoving = supplyDataGrid.SelectedItems.Cast<Supply_View>().ToList();
+            var supplyForRemoving = supplyDataGrid.SelectedItems.Cast<Supply>().ToList();
             if (MessageBox.Show($"Вы точно хотите удалить следующее {supplyForRemoving.Count()} элементов?", "Внимание",
                 MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
             {
                 try
                 {
-                    Real_estate_agencyEntities2.GetContext().Supply_View.RemoveRange(supplyForRemoving);
+                    Real_estate_agencyEntities2.GetContext().Supplies.RemoveRange(supplyForRemoving);
                     Real_estate_agencyEntities2.GetContext().SaveChanges();
                     MessageBox.Show("Данные удалены!");
 
-                    supplyDataGrid.ItemsSource = Real_estate_agencyEntities2.GetContext().Supply_View.ToList();
+                    supplyDataGrid.ItemsSource = Real_estate_agencyEntities2.GetContext().Supplies.ToList();
                 }
                 catch (Exception ex)
                 {
@@ -61,7 +62,7 @@ namespace Agency
             if (Visibility == Visibility.Visible)
             {
                 Real_estate_agencyEntities2.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
-                supplyDataGrid.ItemsSource = Real_estate_agencyEntities2.GetContext().Supply_View.ToList();
+                supplyDataGrid.ItemsSource = Real_estate_agencyEntities2.GetContext().Supplies.ToList();
                 // clientDataGrid.Items.Refresh();
             }
         }
